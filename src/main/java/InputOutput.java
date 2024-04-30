d0d8aca8e24cd5aa3f274fc1a1f48ab6644afd7e
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-01 01:17:48 +0900

    Fix: scanner가 비어있는 이슈
    InputOutput 클래스의 필드인 Scanner에 정의하지 않아서 생긴 문제로 수정함

--- src/main/java/InputOutput.java
+++ src/main/java/InputOutput.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class InputOutput {
-    private Scanner scanner;
+    private Scanner scanner = new Scanner(System.in);
     public void startOutput() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
