2f9be772e2bb4db0121af8981f62c50c3416361e
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-01 01:07:41 +0900

    Feat: 숫자 세자리 입력
    InputOutput 클래스의 numberInput 메서드는 스캐너를 통해 입력 받는
    문자열을 반환

--- src/main/java/InputOutput.java
+++ src/main/java/InputOutput.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class InputOutput {
+    private Scanner scanner;
     public void startOutput() {
         System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    public String numberInput() {
+        return scanner.nextLine();
     }
 }
