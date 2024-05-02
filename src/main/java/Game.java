7beb65fde1428ca88d7896c1c3b9fb409ad44060
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 10:05:47 +0900

    Fix: depth가 3이어서 Game 클래스의 start 메서드를 분리함
    while문 안에 while문 안에 if 문이 존재하여 depth가 3이 되었다.
    프로그래밍 요구사항에 depth를 2까지만 허용한다고 했으므로 숫자 야구
    게임을 한 번 진행하는 경우를 oneGame 메서드로 나눴고, 주석을 달았다.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,20 +6,13 @@ public class Game {
 
     public void start() {
         while (true) {
+            // 임의의 세 자리 숫자 생성
             String random = number.create();
-            while (true) {
-                inputOutput.startOutput();
-                String input = inputOutput.numberInput();
-                if (!validate.numberValidate(input)) {
-                    throw new IllegalArgumentException();
-                }
-                inputOutput.hitterResultOutput(baseballUtil.ball(random, input),
-                        baseballUtil.strike(random, input));
-                if (baseballUtil.strike(random, input) == 3) {
-                    inputOutput.endOutput();
-                    break;
-                }
-            }
+
+            // 숫자 야구 게임 진행
+            oneGame(random);
+
+            // 다시 게임을 진행할 것인지, 종료할 것 인지 여부 출력 및 입력
             inputOutput.restartOutput();
             String yesOrNo = inputOutput.yesOrNoInput();
             if (!validate.yesOrNoValidate(yesOrNo)) {
@@ -29,6 +22,24 @@ public class Game {
                 break;
             }
         }
+    }
 
+    /*
+        한 번의 숫자 야구 게임의 진행
+     */
+    public void oneGame(String random) {
+        while (true) {
+            inputOutput.startOutput();
+            String input = inputOutput.numberInput();
+            if (!validate.numberValidate(input)) {
+                throw new IllegalArgumentException();
+            }
+            inputOutput.hitterResultOutput(baseballUtil.ball(random, input),
+                    baseballUtil.strike(random, input));
+            if (baseballUtil.strike(random, input) == 3) {
+                inputOutput.endOutput();
+                break;
+            }
+        }
     }
 }
